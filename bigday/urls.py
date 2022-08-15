81eea42c1faeced72797c55e123c956181f3b789
Lance Goyke <lancegoyke@gmail.com>
2022-08-14 17:17:32 -0700

    add static to urls.py

--- bigday/urls.py
+++ bigday/urls.py
@@ -1,5 +1,7 @@
-from django.urls import re_path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import re_path, include
 
 
 urlpatterns = [
@@ -7,4 +9,4 @@ urlpatterns = [
     re_path(r'^', include('guests.urls')),
     re_path(r'^admin/', admin.site.urls),
     re_path('^accounts/', include('django.contrib.auth.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
